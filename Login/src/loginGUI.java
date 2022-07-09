e90443cc41f074b09caf92637aba05a817200cd7
richnanney <richardpnanney@gmail.com>
2022-07-09 05:09:13 -0600

    All of the code.
    I wrote the code first, then made the repository incorrectly, so this is me moving the files into the repository. Oops.

---
+++ Login/src/loginGUI.java
@@ -0,0 +1,95 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Scanner;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class loginGUI implements ActionListener{
+    
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passwordLabel;
+    private static JPasswordField passwordText;
+    private static JButton button;
+    private static JLabel success;
+
+    public static void main(String[] args) {
+
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame();
+        frame.setSize(350, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        userLabel = new JLabel("User");
+        userLabel.setBounds(10,20,80,25);
+        panel.add(userLabel);
+
+        userText = new JTextField(20);
+        userText.setBounds(100,20,165,25);
+        panel.add(userText);
+
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        passwordText = new JPasswordField();
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+        button = new JButton("Login");
+        button.setBounds(10, 80, 80, 25);
+        button.addActionListener(new loginGUI());
+        panel.add(button);
+
+        success = new JLabel("");
+        success.setBounds(10, 110, 300, 25);
+        panel.add(success);
+
+        frame.setVisible(true);
+    }
+    private static Scanner x;
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String user = userText.getText();
+        String password = passwordText.getText();
+
+        boolean found = false;
+        String tempUsername = "";
+        String tempPassword = "";
+        
+        try{
+            x = new Scanner(new File("members.txt"));
+            x.useDelimiter("[,\n]");
+            while(x.hasNext() && !found){
+                tempUsername = x.next();
+                tempPassword = x.next();
+
+                if(tempUsername.trim().equals(user.trim()) && tempPassword.trim().equals(password.trim())){
+                    found = true;
+                } 
+
+                
+            }
+        }
+        catch(Exception f)
+        {
+
+        }
+
+        if(found) {
+            success.setText("Login successful!");
+        }
+        else{
+            success.setText("Login incorrect.");
+        }
+    }
+}
